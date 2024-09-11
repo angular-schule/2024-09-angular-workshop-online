8545531f259302a3558d9ceca15708299909bdba
Ferdinand Malcher <ferdinand@malcher.media>
2024-09-11 13:35:25 +0200

    Interface Book und Buchdaten

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  books: Book[] = [];
 
+  constructor() {
+    this.books = [
+      {
+        isbn: '123',
+        title: 'Angular',
+        description: 'Grundlagen und mehr',
+        rating: 5,
+        price: 42.9
+      },
+      {
+        isbn: '456',
+        title: 'Vue.js',
+        description: 'Das grüne Framework',
+        rating: 3,
+        price: 32.9
+      }
+    ];
+  }
 }
