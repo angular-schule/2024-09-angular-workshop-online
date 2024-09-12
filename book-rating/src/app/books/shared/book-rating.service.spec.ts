b6aed32a98281fedae6d20f9396833ca81318f4a
Ferdinand Malcher <ferdinand@malcher.media>
2024-09-12 12:05:37 +0200

    Tests BookRatingService

--- book-rating/src/app/books/shared/book-rating.service.spec.ts
+++ book-rating/src/app/books/shared/book-rating.service.spec.ts
@@ -1,16 +1,57 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BookRatingService } from './book-rating.service';
+import { Book } from './book';
 
 describe('BookRatingService', () => {
   let service: BookRatingService;
+  let book: Book;
 
   beforeEach(() => {
+    // Arrange
     TestBed.configureTestingModule({});
     service = TestBed.inject(BookRatingService);
+    // service = new BookRatingService()
+
+    book = {
+      isbn: '123',
+      title: '',
+      description: '',
+      rating: 3,
+      price: 2
+    };
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should increment the book rating for rateUp', () => {
+    // Arrange
+    book.rating = 3;
+
+    // Act
+    const ratedBook = service.rateUp(book);
+
+    // Assert
+    expect(ratedBook.rating).toBe(4); // NICHT: book.rating + 1
+  });
+
+  it('should decrement the book rating for rateDown', () => {
+    book.rating = 3;
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(2);
+  });
+
+  it('should not rate higher than 5', () => {
+    book.rating = 5;
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(5);
+  });
+
+  it('should not rate lower than 1', () => {
+    book.rating = 1;
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(1);
   });
 });
