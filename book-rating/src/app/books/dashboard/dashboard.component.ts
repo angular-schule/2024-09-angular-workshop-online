708ef2284c727001b39d10dc0fff6e20c39adf6c
Ferdinand Malcher <ferdinand@malcher.media>
2024-09-12 14:20:07 +0200

    Buchliste aktualisieren

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookComponent } from '../book/book.component';
+import { BookRatingService } from '../shared/book-rating.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +11,8 @@ import { BookComponent } from '../book/book.component';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  private rs = inject(BookRatingService);
+
   books: Book[] = [];
 
   constructor() {
@@ -35,10 +38,25 @@ export class DashboardComponent {
 
 
   doRateUp(book: Book) {
-    console.log('UP', book);
+    const ratedBook = this.rs.rateUp(book);
+    this.updateList(ratedBook);
   }
 
   doRateDown(book: Book) {
-    console.log('DOWN', book);
+    const ratedBook = this.rs.rateDown(book);
+    this.updateList(ratedBook);
+  }
+
+  private updateList(ratedBook: Book) {
+    // [1,2,3,4,5].map(e => e * 10) // [10, 20, 30, 40, 50]
+    // [1,2,3,4,5,6,7,8,9].filter(e => e % 2 === 0) // [2,4,6,8]
+
+    this.books = this.books.map(b => {
+      if (b.isbn === ratedBook.isbn) {
+        return ratedBook;
+      } else {
+        return b;
+      }
+    })
   }
 }
