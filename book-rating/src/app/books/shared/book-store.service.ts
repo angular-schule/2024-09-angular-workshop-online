67c56cfabd2980b49ce37322b3fbcc4c133372e6
Ferdinand Malcher <ferdinand@malcher.media>
2024-09-16 13:27:14 +0200

    Environments

--- book-rating/src/app/books/shared/book-store.service.ts
+++ book-rating/src/app/books/shared/book-store.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Book } from './book';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
