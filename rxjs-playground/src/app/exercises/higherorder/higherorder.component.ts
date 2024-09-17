e25400ce6834383a4e00ebf3370fea52af25f948
Ferdinand Malcher <ferdinand@malcher.media>
2024-09-17 14:03:28 +0200

    RxJS

--- rxjs-playground/src/app/exercises/higherorder/higherorder.component.ts
+++ rxjs-playground/src/app/exercises/higherorder/higherorder.component.ts
@@ -31,6 +31,7 @@ export class HigherorderComponent {
     /**************!!**************/
 
     this.result$ = this.source$.pipe(
+      // map(tier => this.es.echo(tier))
     );
 
     /**************!!**************/
