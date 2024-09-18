493142b1e2763e7d3bbc3cd3f266168b7b164780
Ferdinand Malcher <ferdinand@malcher.media>
2024-09-18 16:08:11 +0200

    Zusatzaufgaben Suche

--- book-rating/src/app/books/book-search/book-search.component.ts
+++ book-rating/src/app/books/book-search/book-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { debounceTime, filter, mergeMap, switchMap } from 'rxjs';
+import { debounceTime, filter, map, mergeMap, of, switchMap } from 'rxjs';
 import { BookStoreService } from '../shared/book-store.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 
@@ -19,9 +19,24 @@ export class BookSearchComponent {
   searchResult = toSignal(
     this.searchControl.valueChanges.pipe(
       debounceTime(500),
+      switchMap((term) => {
+        if (term.length >= 3) {
+          return this.bs.search(term);
+        } else {
+          return of([]);
+        }
+      }),
+      // ZUSATZAUFGABE: Buchliste sortieren
+      map(books => [...books].sort((a, b) => b.rating - a.rating))
+    )
+  );
+
+  /*searchResult = toSignal(
+    this.searchControl.valueChanges.pipe(
+      debounceTime(500),
       filter((e) => e.length >= 3),
       switchMap((term) => this.bs.search(term)),
       // ZUSATZAUFGABE: Buchliste sortieren
     )
-  );
+  );*/
 }
