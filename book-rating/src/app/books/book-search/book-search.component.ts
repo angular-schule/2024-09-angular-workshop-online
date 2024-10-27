b8f0b14b8971be0608044fb0a80efe0c33eddb28
Ferdinand Malcher <ferdinand@malcher.media>
2024-10-27 19:05:07 +0100

    Search mit Resource

--- book-rating/src/app/books/book-search/book-search.component.ts
+++ book-rating/src/app/books/book-search/book-search.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, resource } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { debounceTime, filter, mergeMap, switchMap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, mergeMap, switchMap } from 'rxjs';
 import { BookStoreService } from '../shared/book-store.service';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-book-search',
@@ -14,13 +14,16 @@ export class BookSearchComponent {
   searchControl = new FormControl('', { nonNullable: true });
   private bs = inject(BookStoreService);
 
-  // ZUSATZAUFGABE: bei leerem Suchbegriff Liste leeren
-  searchResult = toSignal(
+  private searchTerm = toSignal(
     this.searchControl.valueChanges.pipe(
       debounceTime(500),
       filter((e) => e.length >= 3),
-      switchMap((term) => this.bs.search(term)),
-      // ZUSATZAUFGABE: Buchliste sortieren
+      distinctUntilChanged()
     )
   );
+
+  results = rxResource({
+    request: this.searchTerm,
+    loader: ({ request: term }) => this.bs.search(term)
+  });
 }
